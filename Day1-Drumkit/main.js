f10dcc718a6d374ca2996c91399b7ff3ff446b1b
sagar.maharjan <sagar.maharjan@f1soft.com>
2021-02-18 07:06:50 +0545

    Completed: complete the day 1 task of javascript 30

---
+++ Day1-Drumkit/main.js
@@ -0,0 +1,86 @@
+const keys = document.querySelector('.keys');
+
+document.addEventListener('keydown', (e) => {
+    const keyCode = e.code;
+
+    switch(keyCode) {
+        case 'KeyA':
+            addSound('KeyA');
+            break;
+        case 'KeyS':
+            addSound('KeyS');
+            break;
+        case 'KeyD':
+            addSound('KeyD');
+            break;
+        case 'KeyF':
+            addSound('KeyF');
+            break;
+        case 'KeyG':
+            addSound('KeyG');
+            break;
+        case 'KeyH':
+            addSound('KeyH');
+            break;
+        case 'KeyJ':
+            addSound('KeyJ');
+            break;
+        case 'KeyK':
+            addSound('KeyK');
+            break;
+        case 'KeyL':
+            addSound('KeyL');
+            break;
+        default:
+            console.log('This is from default');
+    }
+})
+
+document.addEventListener('keyup', (e) => {
+    // if (e.propertyName !== 'transform') return;
+    const keyCode = e.code;
+    switch(keyCode) {
+        case 'KeyA':
+            removeSound('KeyA');
+            break;
+        case 'KeyS':
+            removeSound('KeyS');
+            break;
+        case 'KeyD':
+            removeSound('KeyD');
+            break;
+        case 'KeyF':
+            removeSound('KeyF');
+            break;
+        case 'KeyG':
+            removeSound('KeyG');
+            break;
+        case 'KeyH':
+            removeSound('KeyH');
+            break;
+        case 'KeyJ':
+            removeSound('KeyJ');
+            break;
+        case 'KeyK':
+            removeSound('KeyK');
+            break;
+        case 'KeyL':
+            removeSound('KeyL');
+            break;
+        default:
+            console.log('This is from default');
+    }
+})
+
+function addSound(keyCode) {
+    keys.querySelector(`div[data-key=${keyCode}]`).classList.add('playing');
+    document.querySelector(`audio[data-key=${keyCode}]`).play();
+}
+
+function removeSound(keyCode) {
+    keys.querySelector(`div[data-key=${keyCode}]`).classList.remove('playing');
+
+    let audioElement = document.querySelector(`audio[data-key=${keyCode}]`);
+    audioElement.pause();
+    audioElement.currentTime = 0;
+}
